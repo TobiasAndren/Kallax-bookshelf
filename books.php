2ba68114a2ba8f35b64aa16398e691dd9fe3571b
TobiasAndren <tobiasandren@hotmail.com>
2025-03-13 11:07:12 +0100

    added styling to bookshelf

--- books.php
+++ books.php
@@ -1,9 +1,14 @@
 <?php
 
 $bookshelf = [
-    ["title" => "The Shadow of Time", "author" => "Eleanor Wren"],
-    ["title" => "Echoes of the Past", "author" => "Jonathan Fields"],
-    ["title" => "The Forgotten Manuscript", "author" => "Clara Hastings"],
-    ["title" => "A Journey Through Stars", "author" => "David Calloway"],
-    ["title" => "Legends of the Lost City", "author" => "Maxwell Stone"]
+    ["title" => "The Shadow of Time", "author" => "Eleanor Wren", 'genre' => 'romance'],
+    ["title" => "Echoes of the Past", "author" => "Jonathan Fields", 'genre' => 'romance'],
+    ["title" => "The Forgotten Manuscript", "author" => "Clara Hastings", 'genre' => 'thriller'],
+    ["title" => "A Journey Through Stars", "author" => "David Calloway", 'genre' => 'fantasy'],
+    ["title" => "Legends of the Lost City", "author" => "Maxwell Stone", 'genre' => 'fantasy'],
+    ["title" => "Whispers of the Heart", "author" => "Lillian Moore", "genre" => "romance"],
+    ["title" => "The Silent Witness", "author" => "Gregory Kane", "genre" => "thriller"],
+    ["title" => "Moonlight Enchantment", "author" => "Serena Vale", "genre" => "fantasy"],
+    ["title" => "Beneath the Crimson Sky", "author" => "Oliver Sinclair", "genre" => "romance"],
+    ["title" => "Shadows in the Fog", "author" => "Victoria Hart", "genre" => "thriller"]
 ];
--- index.php
+++ index.php
@@ -10,15 +10,16 @@ include __DIR__ . "/books.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kallax Bookshelf</title>
+    <link rel="stylesheet" href="/style.css">
 </head>
 
 <body>
-    <section aria-label="bookshelf">
+    <ul class="bookshelf" aria-label="bookshelf">
         <?php foreach ($bookshelf as $book) { ?>
             <li><?= $book['title'] ?></li>
         <?php
         } ?>
-    </section>
+    </ul>
 </body>
 
 </html>
