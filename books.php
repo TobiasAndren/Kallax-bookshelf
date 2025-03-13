f02f3ae19550def9d54c8d92e9599ed2ea005d07
JosAhl <josefineahlstrand@live.se>
2025-03-13 09:55:02 +0100

    added book array

--- books.php
+++ books.php
@@ -1,1 +1,9 @@
 <?php
+
+$bookshelf = [
+    ["title" => "The Shadow of Time", "author" => "Eleanor Wren"],
+    ["title" => "Echoes of the Past", "author" => "Jonathan Fields"],
+    ["title" => "The Forgotten Manuscript", "author" => "Clara Hastings"],
+    ["title" => "A Journey Through Stars", "author" => "David Calloway"],
+    ["title" => "Legends of the Lost City", "author" => "Maxwell Stone"]
+];
