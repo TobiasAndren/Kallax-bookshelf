93df2e858a82ae6e7a7acaef3ffeaa1735bb03cc
JosAhl <josefineahlstrand@live.se>
2025-03-13 10:41:40 +0100

    added sorting logic

--- index.php
+++ index.php
@@ -2,6 +2,20 @@
 
 include __DIR__ . "/books.php";
 
+if (isset($_GET['sort'])) {
+    $sortOrder = $_GET['sort'];
+
+    if ($sortOrder === "ascTitle") {
+        usort($bookshelf, fn($a, $b) => strcmp($a['title'], $b['title']));
+    } elseif ($sortOrder === "descTitle") {
+        usort($bookshelf, fn($a, $b) => strcmp($b['title'], $a['title']));
+    } elseif ($sortOrder === "ascAuthor") {
+        usort($bookshelf, fn($a, $b) => strcmp($a['author'], $b['author']));
+    } elseif ($sortOrder === "descAuthor") {
+        usort($bookshelf, fn($a, $b) => strcmp($b['author'], $a['author']));
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,12 +27,24 @@ include __DIR__ . "/books.php";
 </head>
 
 <body>
+    <select name="sort" id="sort">
+        <option value="ascTitle">Sort by title ASC</option>
+        <option value="descTitle">Sort by title DESC</option>
+        <option value="ascAuthor">Sort by author ASC</option>
+        <option value="descAuthor">Sort by author DESC</option>
+    </select>
     <section aria-label="bookshelf">
         <?php foreach ($bookshelf as $book) { ?>
-            <li><?= $book['title'] ?></li>
+            <li><?= $book['title'] . ' - ' . $book['author'] ?></li>
         <?php
         } ?>
     </section>
+    <script>
+        document.getElementById('sort').addEventListener('change', function() {
+            const sortValue = this.value;
+            window.location.href = `/index.php?sort=${sortValue}`;
+        });
+    </script>
 </body>
 
 </html>
